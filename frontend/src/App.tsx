77b2bd3e06165ff622bdb7bd3bc55242dab077e6
Phil Zhang <zhangphil@outlook.com>
2026-01-25 02:09:46 -0800

    Auto-commit: Edit modified App.tsx
    - File: C:\Users\zhang\Documents\monkeysnow\frontend\src\App.tsx
    - Tool: Edit
    - Session: a85b89cf-9bce-4ba4-8722-b1f617cab66d

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,7 @@
 import { useState, useEffect, useCallback, useMemo } from 'react';
 import { Header } from './components/Header';
 import { UtilityBar } from './components/UtilityBar';
+import { CompactUtilityBar } from './components/CompactUtilityBar';
 import { FullView } from './components/FullView';
 import { DefaultCard } from './components/DefaultCard';
 import { CompactCard } from './components/cards';
