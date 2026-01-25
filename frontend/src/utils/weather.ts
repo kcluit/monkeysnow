9335a5aa2419c33a8d6a33108534745e4a653a28
Phil Zhang <zhangphil@outlook.com>
2026-01-25 14:27:50 -0800

    Auto-commit: Edit modified weather.ts
    - File: C:\Users\zhang\Documents\monkeysnow\frontend\src\utils\weather.ts
    - Tool: Edit
    - Session: 6afc5831-c75a-4b40-bfd1-02a374614889

--- frontend/src/utils/weather.ts
+++ frontend/src/utils/weather.ts
@@ -105,7 +105,7 @@ export function processResortData(
         weather: mainCondition,
         weatherEmoji: getWeatherEmoji(mainCondition),
         periods,
-        freezingLevel: highestFreezingLevel ? `${Math.round(highestFreezingLevel)} m` : '- m',
+        freezingLevel: formatFreezingLevel(highestFreezingLevel, unitSystem),
         snowCondition
       });
     }
