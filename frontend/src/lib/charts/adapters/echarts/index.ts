8090e860756c8f4a883a834b8465bdea56d32783
Phil Zhang <zhangphil@outlook.com>
2026-01-25 17:32:01 -0800

    Auto-commit: Edit modified index.ts
    - File: C:\Users\zhang\Documents\monkeysnow\frontend\src\lib\charts\adapters\echarts\index.ts
    - Tool: Edit
    - Session: 8e61679e-96a5-41b0-a17a-ebc46dd5e0cd

--- frontend/src/lib/charts/adapters/echarts/index.ts
+++ frontend/src/lib/charts/adapters/echarts/index.ts
@@ -3,6 +3,6 @@
  */
 
 export { EChartsAdapter } from './EChartsAdapter';
-export { getEChartsTheme, buildEChartsThemeOption } from './themeAdapter';
+export { getEChartsTheme } from './themeAdapter';
 export { buildEChartsOption } from './optionBuilder';
 export { buildSeries, buildLineSeries, buildBarSeries, buildAreaSeries } from './seriesBuilders';
