127e88cc5674ff78b523f1998d1e6e595f05cf8d
Phil Zhang <zhangphil@outlook.com>
2026-01-25 16:09:29 -0800

    Auto-commit: Edit modified index.ts
    - File: C:\Users\zhang\Documents\monkeysnow\frontend\src\components\detail\index.ts
    - Tool: Edit
    - Session: b55b5ea0-8b71-493e-929e-017c28fad221

--- frontend/src/components/detail/index.ts
+++ frontend/src/components/detail/index.ts
@@ -1,5 +1,4 @@
 export { DetailedResortView } from './DetailedResortView';
-export { DetailViewHeader } from './DetailViewHeader';
 export { DetailUtilityBar } from './DetailUtilityBar';
 export { DetailChartGrid } from './DetailChartGrid';
 export { WeatherChart } from './WeatherChart';
