49fecd3f1a30ca63278528ee4ef993bdfa4c676b
Phil Zhang <zhangphil@outlook.com>
2026-01-27 13:42:25 -0800

    Auto-commit: Edit modified chartBuilder.ts
    - File: C:\Users\zhang\Documents\monkeysnow\frontend\src\utils\chartBuilder.ts
    - Tool: Edit
    - Session: cb0af0bb-aed1-4b06-9557-30a63b21becb

--- frontend/src/utils/chartBuilder.ts
+++ frontend/src/utils/chartBuilder.ts
@@ -6,13 +6,7 @@
  * chart abstraction layer.
  */
 
-import type {
-    ChartConfig,
-    ChartType,
-    SeriesConfig,
-    ChartTheme,
-    MarkLineData,
-} from '../lib/charts';
+import type { ChartConfig, ChartType, SeriesConfig, ChartTheme } from '../lib/charts';
 import type { WeatherChartProps } from '../types/detailView';
 import type { WeatherModel, HourlyDataPoint, AggregationType } from '../types/openMeteo';
 import type { UnitSystem } from '../types';
