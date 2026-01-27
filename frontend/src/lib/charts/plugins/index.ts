9e675878b0e985a32201dbd79dd81f44e924187c
Phil Zhang <zhangphil@outlook.com>
2026-01-27 12:19:42 -0800

    Auto-commit: Edit modified index.ts
    - File: C:\Users\zhang\Documents\monkeysnow\frontend\src\lib\charts\plugins\index.ts
    - Tool: Edit
    - Session: 496451f2-e319-4ff8-ac8d-ef9ccc823230

--- frontend/src/lib/charts/plugins/index.ts
+++ frontend/src/lib/charts/plugins/index.ts
@@ -7,6 +7,6 @@
 export { createZoomPlugin, type ZoomPluginOptions } from './zoomPlugin';
 export { createTooltipPlugin, type TooltipPluginOptions } from './tooltipPlugin';
 export { createLegendPlugin, type LegendPluginOptions } from './legendPlugin';
-export { createSyncPlugin, syncAllCharts, resetAllCharts, type SyncPluginOptions } from './syncPlugin';
+
 export { createMarkLinesPlugin, type MarkLinesPluginOptions } from './markLinesPlugin';
 export { createBandFillPlugin, type BandFillPluginOptions } from './bandFillPlugin';
