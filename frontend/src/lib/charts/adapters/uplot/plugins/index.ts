022d242906c42df5923ea7c212a3ca9ec7bcd25d
Phil Zhang <zhangphil@outlook.com>
2026-01-27 00:44:39 -0800

    Auto-commit: Edit modified index.ts
    - File: C:\Users\zhang\Documents\monkeysnow\frontend\src\lib\charts\adapters\uplot\plugins\index.ts
    - Tool: Edit
    - Session: 900aedc7-0d71-41fb-8e47-d4bad7ea9378

--- frontend/src/lib/charts/adapters/uplot/plugins/index.ts
+++ frontend/src/lib/charts/adapters/uplot/plugins/index.ts
@@ -5,3 +5,6 @@
 export { markLinesPlugin } from './markLinesPlugin';
 export { legendPlugin } from './legendPlugin';
 export { zoomPlugin } from './zoomPlugin';
+export { tooltipPlugin } from './tooltipPlugin';
+export { bandFillPlugin } from './bandFillPlugin';
+export { labelsPlugin } from './labelsPlugin';
