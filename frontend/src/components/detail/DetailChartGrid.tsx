a07a233592dc698c54758ff85d711373721056dc
Phil Zhang <zhangphil@outlook.com>
2026-01-26 23:50:41 -0800

    Auto-commit: Edit modified DetailChartGrid.tsx
    - File: C:\Users\zhang\Documents\monkeysnow\frontend\src\components\detail\DetailChartGrid.tsx
    - Tool: Edit
    - Session: 8e7db1e5-4ef1-4bdb-a3dc-da465e443aa4

--- frontend/src/components/detail/DetailChartGrid.tsx
+++ frontend/src/components/detail/DetailChartGrid.tsx
@@ -102,6 +102,7 @@ function DetailChartGridInner({
                     selectedModels={selectedModels}
                     selectedAggregations={selectedAggregations}
                     aggregationColors={aggregationColors}
+                    hideAggregationMembers={hideAggregationMembers}
                     variable={variable}
                     unitSystem={unitSystem}
                     timezoneInfo={timezoneInfo}
