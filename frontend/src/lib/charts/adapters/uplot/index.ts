7cd997da1c9ef060dee1a820dca7f1088b7993da
Phil Zhang <zhangphil@outlook.com>
2026-01-27 01:40:43 -0800

    refactor: remove chat API route.

--- frontend/src/lib/charts/adapters/uplot/index.ts
+++
@@ -1,8 +0,0 @@
-/**
- * uPlot Adapter - Public Exports
- */
-
-export { UPlotAdapter } from './UPlotAdapter';
-export { getUPlotTheme } from './themeAdapter';
-export { buildUPlotOptions } from './optionBuilder';
-export { buildUPlotData } from './dataBuilder';
