da50cee395a10d4d3bd12f6a20fc386dd211fbca
Phil Zhang <zhangphil@outlook.com>
2026-01-28 01:38:06 -0800

    Auto-commit: Edit modified ModelSelectionGridModal.tsx
    - File: C:\Users\zhang\Documents\monkeysnow\frontend\src\components\ModelSelectionModal\ModelSelectionGridModal.tsx
    - Tool: Edit
    - Session: 30e6299a-3ced-4643-8bf8-f6c333b76521

--- frontend/src/components/ModelSelectionModal/ModelSelectionGridModal.tsx
+++ frontend/src/components/ModelSelectionModal/ModelSelectionGridModal.tsx
@@ -6,6 +6,8 @@
  */
 
 import { useState, useEffect, useRef, useMemo, useCallback, memo } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons';
 import {
   buildModelHierarchyTree,
   getModelsUnderNode,
