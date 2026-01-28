1d05fbe3df770f8981f2f916b52673b098c953be
Phil Zhang <zhangphil@outlook.com>
2026-01-27 18:42:32 -0800

    Auto-commit: Edit modified index.ts
    - File: C:\Users\zhang\Documents\monkeysnow\frontend\src\lib\charts\plugins\index.ts
    - Tool: Edit
    - Session: 80c71a81-866a-44dc-9daa-931a054e0b23

--- frontend/src/lib/charts/plugins/index.ts
+++ frontend/src/lib/charts/plugins/index.ts
@@ -7,3 +7,4 @@
 export { createZoomPlugin, type ZoomPluginOptions } from './zoomPlugin';
 export { createBandFillPlugin, type BandFillPluginOptions } from './bandFillPlugin';
 export { createZeroAxisPlugin, type ZeroAxisPluginOptions } from './zeroAxisPlugin';
+export { createBoxWhiskerPlugin, type BoxWhiskerPluginOptions } from './boxWhiskerPlugin';
