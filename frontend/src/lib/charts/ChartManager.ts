a3dbb9f7dd40c0f9f60d63c4569389b100c7f9db
Phil Zhang <zhangphil@outlook.com>
2026-01-28 01:12:20 -0800

    Auto-commit: Edit modified ChartManager.ts
    - File: C:\Users\zhang\Documents\monkeysnow\frontend\src\lib\charts\ChartManager.ts
    - Tool: Edit
    - Session: 5ac39f7c-f750-4a09-8909-19f38a6de9f1

--- frontend/src/lib/charts/ChartManager.ts
+++ frontend/src/lib/charts/ChartManager.ts
@@ -8,7 +8,7 @@
 import uPlot from 'uplot';
 import 'uplot/dist/uPlot.min.css';
 import type { ChartConfig } from './types';
-import { createZoomPlugin, createBandFillPlugin, createBoxWhiskerPlugin, createHeatmapPlugin, createWindArrowPlugin, createZeroAxisPlugin, createTooltipPlugin, createLegendPlugin } from './plugins';
+import { createZoomPlugin, createBandFillPlugin, createBoxWhiskerPlugin, createHeatmapPlugin, createWindArrowPlugin, createZeroAxisPlugin, createElevationLinesPlugin, createTooltipPlugin, createLegendPlugin } from './plugins';
 import { colorWithOpacity } from './utils/colorUtils';
 import {
     subscribeToZoomChanges,
