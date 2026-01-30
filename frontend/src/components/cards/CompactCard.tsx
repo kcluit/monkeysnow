df902d8171e547c338e550711f3ffd37fc734d47
Phil Zhang <zhangphil@outlook.com>
2026-01-29 20:05:20 -0800

    Auto-commit: Edit modified CompactCard.tsx
    - File: C:\Users\zhang\Documents\monkeysnow\frontend\src\components\cards\CompactCard.tsx
    - Tool: Edit
    - Session: 1e49d4f7-feb2-4e1c-8a2e-230eb670963c

--- frontend/src/components/cards/CompactCard.tsx
+++ frontend/src/components/cards/CompactCard.tsx
@@ -1,6 +1,6 @@
 import { useRef, useState, MouseEvent } from 'react';
 import { webcamUrls } from '../../utils/constants';
-import { calculateDayStats, formatWeatherText, getTemperatureClass, getSnowClass, getWindClass } from './cardUtils';
+import { calculateDayStats, formatWeatherText, getTemperatureStyle, getSnowClass, getWindClass } from './cardUtils';
 import { formatTemp, formatSnow, formatWind } from '../../utils/unitConversion';
 import type { CardProps } from '../../types';
 
