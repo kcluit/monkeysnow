9492d48fa12166eb2346efcfba827c577adda51d
Phil Zhang <zhangphil@outlook.com>
2026-01-31 16:07:44 -0800

    Auto-commit: Edit modified ModelSelectionGridModal.tsx
    - File: C:\Users\zhang\Documents\monkeysnow\frontend\src\components\ModelSelectionModal\ModelSelectionGridModal.tsx
    - Tool: Edit
    - Session: 953a2cb5-d92b-4238-b99c-e2769ab52ede

--- frontend/src/components/ModelSelectionModal/ModelSelectionGridModal.tsx
+++ frontend/src/components/ModelSelectionModal/ModelSelectionGridModal.tsx
@@ -467,16 +467,6 @@ export const ModelSelectionGridModal = memo(function ModelSelectionGridModal({
     });
   }, []);
 
-  // Expand all nodes
-  const handleExpandAll = useCallback(() => {
-    setExpandedNodes(new Set(allNodeIds));
-  }, [allNodeIds]);
-
-  // Collapse all nodes
-  const handleCollapseAll = useCallback(() => {
-    setExpandedNodes(new Set());
-  }, []);
-
   // Auto-focus input on open
   useEffect(() => {
     if (isOpen && inputRef.current) {
