0c9db20958614bd3abbf6734edaade002a898d55
Phil Zhang <zhangphil@outlook.com>
2026-02-08 21:12:36 -0800

    Auto-commit: Edit modified DetailedResortView.tsx
    - File: C:\Users\zhang\Documents\monkeysnow\frontend\src\components\detail\DetailedResortView.tsx
    - Tool: Edit
    - Session: 2650e500-e4d2-4670-8d4c-bb327fe4d59e

--- frontend/src/components/detail/DetailedResortView.tsx
+++ frontend/src/components/detail/DetailedResortView.tsx
@@ -14,6 +14,7 @@ import type { UnitSystem } from '../../types';
 interface DetailedResortViewPropsWithUnits extends DetailedResortViewProps {
     unitSystem: UnitSystem;
     showUtilityBar: boolean;
+    utilityBarStyle: import('../../types').UtilityBarStyle;
     onBack: () => void;
 }
 
