f99fc8a1b3d28280a998acef18ba7b017db19e46
Phil Zhang <zhangphil@outlook.com>
2026-03-03 22:38:45 -0800

    Auto-commit: Edit modified DetailedResortView.tsx
    - File: C:\Users\zhang\Documents\monkeysnow\frontend\src\components\detail\DetailedResortView.tsx
    - Tool: Edit
    - Session: b4a5e2bc-289e-4f61-af49-96d3414c043c

--- frontend/src/components/detail/DetailedResortView.tsx
+++ frontend/src/components/detail/DetailedResortView.tsx
@@ -16,6 +16,7 @@ interface DetailedResortViewPropsWithUnits extends DetailedResortViewProps {
     unitSystem: UnitSystem;
     showUtilityBar: boolean;
     utilityBarStyle: import('../../types').UtilityBarStyle;
+    modelLineOpacity?: ModelLineOpacity;
     onBack: () => void;
 }
 
